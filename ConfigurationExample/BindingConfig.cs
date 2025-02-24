444cc611497ba584b2e3e12c67cb5a44233f4c8c
Nguyen Nhut Huy <nguyennhuthuy.dev@gmail.com>
2025-02-24 22:55:15 +0700

    Add: ConfigurationExample

---
+++ ConfigurationExample/BindingConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace ConfigurationExample
+{
+    internal class BindingConfig
+    {
+        public string StringConfig { get; set; } = string.Empty;
+        public int IntegerConfig { get; set; }
+        public bool BoolConfig { get; set; }
+    }
+}
